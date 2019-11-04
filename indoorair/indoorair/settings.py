5d042343fb322435b1eb9b84f0580b9a1d9fedaa
unknown <bmika@icloud.com>
2019-11-04 16:50:27 -0500

    Finished task 4 and task 5.

--- indoorair/indoorair/settings.py
+++ indoorair/indoorair/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'homepage.apps.HomepageConfig',
+    'gateway.apps.GatewayConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
